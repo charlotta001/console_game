6290cd26e8b3989e0a2f4866eaa22a654daf6951
Hafizh Nurfitrian Syah <hafizhvtuber@gmail.com>
2026-03-16 14:52:43 +0700

    cant reverse moving and tembus tembok

--- console_game/practice/movement.cpp
+++ console_game/practice/movement.cpp
@@ -4,12 +4,18 @@
 #include <conio.h>
 #include <cctype>
 
-int headX, headY; 
+int headX[2], headY[2]; 
 int WIDTH = 30, HEIGHT = 15;
 
 bool gameLoop = false;
 int tailX[100],tailY[100];
 int tailLen = 5;
+
+int foodX, foodY;
+int newdirX = 0, newdirY  =0;
+int curdirX = 0, curdirY  =0;
+//srand(times(0));
+
 
 
 enum Direction{
@@ -44,14 +50,19 @@ int main(){
 
 void setup(){
 
-    headX = WIDTH / 2; // 30
-    headY = HEIGHT / 2; // 15
+    headX[0] = WIDTH / 2; // 30
+    headY[0] = HEIGHT / 2; // 15
 
     for(int i = 0; i < 5 ; i++){
         tailX[i] = (WIDTH / 2 );
         tailY[i] = (HEIGHT / 2) + (i+1);
     }
     dir = STOP;
+    foodX =rand() % (WIDTH - 2 ) + 1;
+    foodY= rand() % (HEIGHT - 2 ) + 1;
+
+    curdirX = 0;
+    curdirY = 0;
     
 }
 
@@ -66,16 +77,18 @@ void draw(){
             // kanan kiri
             } else if(j == 0 || j == WIDTH - 1){
                 std::cout << "*";
-            }else if(i == headY && j == headX){
+            }else if(i == headY[0] && j == headX[0]){
                 std::cout << "K";
             
+            } else if(i == foodY && j == foodX){
+                std::cout << "M";
             } else {
-    bool isTail = false;
-    for(int k = 0; k < tailLen; k++){
-        if(tailX[k] == j && tailY[k] == i){
-            isTail = true;
-            break;
-        }
+                bool isTail = false;
+                for(int k = 0; k < tailLen; k++){
+                    if(tailX[k] == j && tailY[k] == i){
+                        isTail = true;
+                        break;
+            }
     }
     std::cout << (isTail ? "o" : " ");
 }
@@ -88,10 +101,35 @@ void draw(){
 void input(){
     if(_kbhit()){
         switch(tolower(_getch())){
-            case 'w': dir = UP; break;
-            case 'a': dir = LEFT; break;
-            case 's': dir = DOWN; break;
-            case 'd': dir = RIGHT; break;
+            case 'w': 
+            {
+                dir = UP; 
+                newdirX = 0;
+                newdirY = -1;
+                break;
+            }
+            case 'a': 
+            {
+                dir = LEFT; 
+                newdirX = -1;
+                newdirY = 0;
+                break;
+            }    
+            case 's': 
+            {
+                dir = DOWN; 
+                newdirX  = 0;
+                newdirY = 1;
+                break;
+            }
+            case 'd': 
+            {
+                dir = RIGHT; 
+                newdirX = 1;
+                newdirY = 0;
+                break;
+            }
+        
         }
     }
     
@@ -106,23 +144,40 @@ void update(){
     }
 
     if( tailLen > 0){
-        tailX[0] = headX;
-        tailY[0] = headY;
+        tailX[0] = headX[0];
+        tailY[0] = headY[0];
     }
+
+    bool isReverse = (newdirX  == -curdirX && newdirY  == -curdirY);
+    bool isStopped = (curdirX == 0 && curdirY  == 0);
+
+    if(!isReverse || isStopped){
+        curdirX = newdirX;
+        curdirY = newdirY;
+    }
+
+    headX[0] += curdirX;
+    headY[0] += curdirY;
 }
 
-    if(dir == UP) headY--;
-    if(dir == DOWN) headY++;
-    if(dir == LEFT) headX--;
-    if(dir == RIGHT) headX++;
 
-    if(headX == 0){
-        headX = WIDTH - 2;
-    } else if(headX == WIDTH  -1){
-        headX = 1 ;
-    } else if(headY == 0){
-        headY = HEIGHT - 2;
-    } else if(headY == HEIGHT - 1){
-        headY  = 1;
+    
+
+
+    if(headX[0] == 0){
+        headX[0] = WIDTH - 2;
+    } else if(headX[0] == WIDTH  -1){
+        headX[0] = 1 ;
+    } else if(headY[0] == 0){
+        headY[0] = HEIGHT - 2;
+    } else if(headY[0] == HEIGHT - 1){
+        headY[0]  = 1;
     }
+
+    if(headX[0] ==  foodX && headY[0] == foodY){
+        tailLen++;
+        foodX = rand() % (WIDTH -2) + 1;
+        foodY = rand() % (HEIGHT -2) + 1;
+    }
+
 }
